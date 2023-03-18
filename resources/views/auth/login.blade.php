903b267d171d32629060da5f37a26e8c26dfbde1
ridwan780 <ridwan.psp2018@gmail.com>
2023-03-18 22:39:10 +0700

    file login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,11 +8,11 @@
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="theme-color" content="#000000">
-    <title>Mobilekit Mobile UI Kit</title>
+    <title>E-Presensi Geolocation</title>
     <meta name="description" content="Mobilekit HTML Mobile UI Kit">
     <meta name="keywords" content="bootstrap 4, mobile template, cordova, phonegap, mobile, html" />
-    <link rel="icon" type="image/png" href="assets/img/favicon.png" sizes="32x32">
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/icon/192x192.png">
+    <link rel="icon" type="image/png" href="{{asset('assets/img/favicon.png')}}" sizes="32x32">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/img/icon/192x192.png')}}">
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
     <link rel="manifest" href="__manifest.json">
 </head>
@@ -80,18 +80,18 @@
 
     <!-- ///////////// Js Files ////////////////////  -->
     <!-- Jquery -->
-    <script src="assets/js/lib/jquery-3.4.1.min.js"></script>
+    <script src="{{asset('assets/js/lib/jquery-3.4.1.min.js')}}"></script>
     <!-- Bootstrap-->
-    <script src="assets/js/lib/popper.min.js"></script>
-    <script src="assets/js/lib/bootstrap.min.js"></script>
+    <script src="{{asset('assets/js/lib/popper.min.js')}}"></script>
+    <script src="{{asset('assets/js/lib/bootstrap.min.js')}}"></script>
     <!-- Ionicons -->
     <script type="module" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.js"></script>
     <!-- Owl Carousel -->
-    <script src="assets/js/plugins/owl-carousel/owl.carousel.min.js"></script>
+    <script src="{{asset('assets/js/plugins/owl-carousel/owl.carousel.min.js')}}"></script>
     <!-- jQuery Circle Progress -->
-    <script src="assets/js/plugins/jquery-circle-progress/circle-progress.min.js"></script>
+    <script src="{{asset('assets/js/plugins/jquery-circle-progress/circle-progress.min.js')}}"></script>
     <!-- Base Js File -->
-    <script src="assets/js/base.js"></script>
+    <script src="{{asset('assets/js/base.js')}}"></script>
 
 
 </body>
